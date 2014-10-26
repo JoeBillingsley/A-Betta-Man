f544e3f9c8ae607adc522e721493ab0a7be0b8db
JoeTheAverage <billingsleyjoseph@gmail.com>
2014-10-26 09:48:18 +0000

    a

--- ABettaMan/app/src/main/java/whosbetta/whosbetta/whosbest/abettaman/FaceStream.java
+++ ABettaMan/app/src/main/java/whosbetta/whosbetta/whosbest/abettaman/FaceStream.java
@@ -42,10 +42,6 @@ public class FaceStream extends Activity {
     private Camera camera;
 
     private int currentCamera;
-    private static final String SERVER_BASE = "10.8.166.92/ABettaMan/Servlet:8080";
-    private static final int REQUEST_CODE = Menu.FIRST;
-    private AsyncHttpClient client = new AsyncHttpClient();
-    private String clientToken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,47 +73,6 @@ public class FaceStream extends Activity {
         CameraPreview cameraPreview = new CameraPreview(this, camera);
         addContentView(cameraPreview, new ViewGroup.LayoutParams(-1, -1));
         addContentView(listener, new ViewGroup.LayoutParams(-1, -1));
-
-        getToken();
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (resultCode == BraintreePaymentActivity.RESULT_OK) {
-            String paymentMethodNonce = data.getStringExtra(BraintreePaymentActivity.EXTRA_PAYMENT_METHOD_NONCE);
-            RequestParams requestParams = new RequestParams();
-            requestParams.put("payment_method_nonce", paymentMethodNonce);
-            requestParams.put("amount", "10.00");
-            client.post(SERVER_BASE + "/payment", requestParams, new AsyncHttpResponseHandler() {
-                @Override
-                public void onSuccess(String content) {
-                    Toast.makeText(FaceStream.this, content, Toast.LENGTH_LONG).show();
-                }
-            });
-        }
-    }
-
-    public void onStartClick(View view){
-        Customization customization = new Customization.CustomizationBuilder()
-                .primaryDescription("Awesome payment")
-                .secondaryDescription("Using the Client SDK")
-                .amount("$10.00")
-                .submitButtonText("Pay")
-                .build();
-        Intent intent = new Intent(this, BraintreePaymentActivity.class);
-        intent.putExtra(BraintreePaymentActivity.EXTRA_CUSTOMIZATION, customization);
-        intent.putExtra(BraintreePaymentActivity.EXTRA_CLIENT_TOKEN, clientToken);
-        startActivityForResult(intent, REQUEST_CODE);
-    }
-
-    private void getToken() {
-        client.get(SERVER_BASE + "/token", new AsyncHttpResponseHandler() {
-            @Override
-            public void onSuccess(String content) {
-                clientToken = content;
-                findViewById(R.id.btn_start).setEnabled(true);
-            }
-        });
     }
 
     public void test() {
--- ABettaMan/app/src/main/java/whosbetta/whosbetta/whosbest/abettaman/shop.java
+++ ABettaMan/app/src/main/java/whosbetta/whosbetta/whosbest/abettaman/shop.java
@@ -1,19 +1,76 @@
 package whosbetta.whosbetta.whosbest.abettaman;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
+import com.braintreepayments.api.dropin.BraintreePaymentActivity;
+import com.braintreepayments.api.dropin.Customization;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
 
 public class shop extends Activity {
+
+    private static final String SERVER_BASE = "http://10.8.166.92:8080/ABettaMan/";
+    private static final int REQUEST_CODE = Menu.FIRST;
+    private AsyncHttpClient client = new AsyncHttpClient();
+    private String clientToken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
+
+        getToken();
+
+        Log.i("Well", SERVER_BASE + "clienttoken");
+        Log.i("Woooh", String.valueOf(clientToken == null));
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == BraintreePaymentActivity.RESULT_OK) {
+            String paymentMethodNonce = data.getStringExtra(BraintreePaymentActivity.EXTRA_PAYMENT_METHOD_NONCE);
+            RequestParams requestParams = new RequestParams();
+            requestParams.put("payment_method_nonce", paymentMethodNonce);
+            requestParams.put("amount", "10.00");
+            client.post(SERVER_BASE + "/payment", requestParams, new AsyncHttpResponseHandler() {
+                @Override
+                public void onSuccess(String content) {
+                    Toast.makeText(shop.this, "Wooh", Toast.LENGTH_LONG);
+                }
+            });
+        }
+    }
+    public void onStartClick(View view){
+        Customization customization = new Customization.CustomizationBuilder()
+                .primaryDescription("Awesome payment")
+                .secondaryDescription("Using the Client SDK")
+                .amount("$10.00")
+                .submitButtonText("Pay")
+                .build();
+        Intent intent = new Intent(this, BraintreePaymentActivity.class);
+        intent.putExtra(BraintreePaymentActivity.EXTRA_CUSTOMIZATION, customization);
+        intent.putExtra(BraintreePaymentActivity.EXTRA_CLIENT_TOKEN, clientToken);
+        startActivityForResult(intent, REQUEST_CODE);
+    }
+
+
+    private void getToken() {
+        client.get(SERVER_BASE + "clienttoken", new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(String content) {
+                clientToken = content;
+                findViewById(R.id.btn_start).setEnabled(true);
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
