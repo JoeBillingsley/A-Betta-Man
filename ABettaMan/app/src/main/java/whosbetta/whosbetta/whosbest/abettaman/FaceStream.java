e2b55facbe3d89b261bf2eb13923888ea969bc0d
stats94 <stats1994@live.co.uk>
2014-10-26 02:42:48 +0000

    Input Braintree Methods
    I HAVE NO IDEA WHAT ANY OF THIS DOES

--- ABettaMan/app/src/main/java/whosbetta/whosbetta/whosbest/abettaman/FaceStream.java
+++ ABettaMan/app/src/main/java/whosbetta/whosbetta/whosbest/abettaman/FaceStream.java
@@ -4,10 +4,23 @@ import android.app.Activity;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.TextView;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
+
+import com.braintreepayments.api.dropin.BraintreePaymentActivity;
+import com.braintreepayments.api.dropin.Customization;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
 
 import whosbetta.whosbetta.whosbest.abettaman.util.SystemUiHider;
 
@@ -29,6 +42,10 @@ public class FaceStream extends Activity {
     private Camera camera;
 
     private int currentCamera;
+    private static final String SERVER_BASE = "";
+    private static final int REQUEST_CODE = Menu.FIRST;
+    private AsyncHttpClient client = new AsyncHttpClient();
+    private String clientToken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,6 +77,50 @@ public class FaceStream extends Activity {
         CameraPreview cameraPreview = new CameraPreview(this, camera);
         addContentView(cameraPreview, new ViewGroup.LayoutParams(-1, -1));
         addContentView(listener, new ViewGroup.LayoutParams(-1, -1));
+
+        getToken();
+    }
+
+    //I have no idea what this does
+//    @Override
+    protected void onActivityResultMethod(int requestCode, int resultCode, Intent data) {
+        if (resultCode == BraintreePaymentActivity.RESULT_OK) {
+            String paymentMethodNonce = data.getStringExtra(BraintreePaymentActivity.EXTRA_PAYMENT_METHOD_NONCE);
+            RequestParams requestParams = new RequestParams();
+            requestParams.put("payment_method_nonce", paymentMethodNonce);
+            requestParams.put("amount", "10.00");
+            client.post(SERVER_BASE + "/payment", requestParams, new AsyncHttpResponseHandler() {
+                @Override
+                public void onSuccess(String content) {
+                    Toast.makeText(FaceStream.this, content, Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+    }
+
+    //I have no idea what this does
+    public void onStartClickMethod(View view){
+        Customization customization = new Customization.CustomizationBuilder()
+                .primaryDescription("Awesome payment")
+                .secondaryDescription("Using the Client SDK")
+                .amount("$10.00")
+                .submitButtonText("Pay")
+                .build();
+        Intent intent = new Intent(this, BraintreePaymentActivity.class);
+        intent.putExtra(BraintreePaymentActivity.EXTRA_CUSTOMIZATION, customization);
+        intent.putExtra(BraintreePaymentActivity.EXTRA_CLIENT_TOKEN, clientToken);
+        startActivityForResult(intent, REQUEST_CODE);
+    }
+
+    //I have no idea what this does
+    private void getToken() {
+        client.get(SERVER_BASE + "/token", new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(String content) {
+                clientToken = content;
+                findViewById(R.id.btn_start).setEnabled(true);
+            }
+        });
     }
 
     public void test() {
@@ -71,6 +132,9 @@ public class FaceStream extends Activity {
     @Override
     public void onResume() {
         super.onResume();
+
+//        mBraintree.addListener(this);
+//        mBraintree.unlockListeners();
     }
 
     @Override
@@ -82,6 +146,9 @@ public class FaceStream extends Activity {
             camera.release();
             camera = null;
         }
+
+//        mBraintree.lockListeners();
+//        mBraintree.removeListener(this);
     }
 
     private Camera getCamera() {
